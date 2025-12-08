23246d43608bc673765dba272c3f932cc03c4886
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-12-08 15:14:14 -0600

    more changes

--- js_crime.js
+++ js_crime.js
@@ -1,16 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const stateEntry = document.querySelector('#stateIn')
+    const yearEntry = document.querySelector('#yearIn');
     const button = document.querySelector('#submitButton');
     button.addEventListener('click', (e) => {
         e.preventDefault();
         const state = stateEntry.value;
-        loadStateInfo(state);
+        const year = yearEntry.value;
+        loadStateInfo(state, year);
     })
 });
 
-async function loadStateInfo(state) {
-    const url = "https://api.usa.gov/crime/fbi/cde/arrest/state/" + state + "/all?type=totals&from=01-2025&to=12-2025&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+async function loadStateInfo(state, year) {
+    const url = "https://api.usa.gov/crime/fbi/cde/arrest/state/" + state + "/all?type=totals&" + year + "&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
     let response = await fetch(url);
     if(response.ok) {
         let report = await response.json();
@@ -21,6 +23,30 @@ async function loadStateInfo(state) {
         const offenseBreakdown = report['Offense Breakdown'];
         const offenseCategory = report['Offense Category'];
         const offenseName = report['Offense Name'];
+
+        const canvas = document.getElementById('crimeCanvas');
+        const ctx = canvas.getContext('2d');
+
+        new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: maleArrestsByAge,
+            datasets: [{
+            backgroundColor: 'blue',
+            data: offenseBreakdown
+            }]
+        },
+        options: {
+            plugins: {
+            legend: {display: false},
+            title: {
+                display: true,
+                text: "Crime Offense Breakdown",
+                font: {size: 16}
+            }
+            }
+        }
+        });
         console.log(report);
     } 
     
