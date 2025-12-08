df0b0e51a667e6854ff1252ae630057cff51d02b
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-12-08 14:37:43 -0600

    added some stuff

--- js_crime.js
+++ js_crime.js
@@ -11,9 +11,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function loadStateInfo(state) {
     const url = "https://api.usa.gov/crime/fbi/cde/arrest/state/" + state + "/all?type=totals&from=01-2025&to=12-2025&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
-    let responseFromZy = await fetch(url);
-    if(responseFromZy.ok) {
-        let report = await responseFromZy.json();
+    let response = await fetch(url);
+    if(response.ok) {
+        let report = await response.json();
+        const arresteeRace = report['Arrestee Race']; 
+        const arresteeSex = report['Arrestee Sex'];
+        const femaleArrestsByAge = report['Female Arrests by Age'];
+        const maleArrestsByAge = report['Male Arrests by Age'];
+        const offenseBreakdown = report['Offense Breakdown'];
+        const offenseCategory = report['Offense Category'];
+        const offenseName = report['Offense Name'];
+        console.log(report);
+    } 
+    
+    else {
+        console.error("HTTP-Error: " + response.status);
+    }
+}
+
+
+
+async function loadCityInfo(city) {
+    const url = "Not avaiable yet";
+    let response = await fetch(url);
+    if(response.ok) {
+        let report = await response.json();
         console.log(report);
     }
 }
