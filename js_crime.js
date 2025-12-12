30b8411d941f6fce7b6d3dfc40d2fc90cf1a3f7e
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-12-12 12:25:17 -0600

    more fixes

--- js_crime.js
+++ js_crime.js
@@ -75,15 +75,15 @@ async function loadCityInfo(city) {
 async function loadStateInfo2(state) {
 
     const stateRow = {
-        AL: "1", AK: "2", AZ: "4", AR: "5", CA: "6", CO: "8",
-        CT: "9", DE: "10", FL: "12", GA: "13", HI: "15", ID: "16",
-        IL: "17", IN: "18", IA: "19", KS: "20", KY: "21", LA: "22",
-        ME: "23", MD: "24", MA: "25", MI: "26", MN: "27", MS: "28",
-        MO: "29", MT: "30", NE: "31", NV: "32", NH: "33", NJ: "34",
-        NM: "35", NY: "36", NC: "37", ND: "38", OH: "39", OK: "40",
-        OR: "41", PA: "42", RI: "44", SC: "45", SD: "46", TN: "47",
-        TX: "48", UT: "49", VT: "50", VA: "51", WA: "53", WV: "54",
-        WI: "55", WY: "56"
+        AL: "1", AK: "2", AZ: "3", AR: "4", CA: "5", CO: "6",
+        CT: "7", DE: "8", FL: "10", GA: "11", HI: "12", ID: "13",
+        IL: "14", IN: "15", IA: "16", KS: "17", KY: "18", LA: "19",
+        ME: "20", MD: "21", MA: "22", MI: "23", MN: "24", MS: "25",
+        MO: "26", MT: "27", NE: "28", NV: "29", NH: "30", NJ: "31",
+        NM: "32", NY: "33", NC: "34", ND: "35", OH: "36", OK: "37",
+        OR: "38", PA: "39", RI: "40", SC: "41", SD: "42", TN: "43",
+        TX: "44", UT: "45", VT: "46", VA: "47", WA: "48", WV: "49",
+        WI: "50", WY: "51"
     }
 
     const row = stateRow[state];
