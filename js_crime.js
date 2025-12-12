0e0261553a5cd97928e48fffebc569dc1c838bfd
ConnorBeachler835 <beachlerc@lopers.unk.edu>
2025-12-12 11:17:21 -0600

    Test 12343895607324690534-752

--- js_crime.js
+++ js_crime.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const state = stateEntry.value;
         const year = yearEntry.value;
         loadStateInfo(state, year);
+        loadStateInfo2(state)
     })
 });
 
@@ -71,3 +72,37 @@ async function loadCityInfo(city) {
     }
 }
 
+async function loadStateInfo2(state) {
+
+    const stateFIPS = {
+        AL: "01", AK: "02", AZ: "04", AR: "05", CA: "06", CO: "08",
+        CT: "09", DE: "10", FL: "12", GA: "13", HI: "15", ID: "16",
+        IL: "17", IN: "18", IA: "19", KS: "20", KY: "21", LA: "22",
+        ME: "23", MD: "24", MA: "25", MI: "26", MN: "27", MS: "28",
+        MO: "29", MT: "30", NE: "31", NV: "32", NH: "33", NJ: "34",
+        NM: "35", NY: "36", NC: "37", ND: "38", OH: "39", OK: "40",
+        OR: "41", PA: "42", RI: "44", SC: "45", SD: "46", TN: "47",
+        TX: "48", UT: "49", VT: "50", VA: "51", WA: "53", WV: "54",
+        WI: "55", WY: "56"
+    }
+
+    const fips = stateFIPs[state];
+
+    const url = "https://api.census.gov/data/2020/dec/cd118?get=group(P1)&ucgid=pseudo(0100000US$0400000)";
+
+    let response = await fetch(url);
+
+    if(response.ok) {
+        let data = await response.json();
+
+        const stateName=data[1][0];
+        const population = parseInt(data[1][1]).toLocaleString();
+
+        document.querySelector("#statePopulation").textContent =
+            `${stateName} Population (2020 Census): ${population}`;
+    }
+    else {
+        document.querySelector("#statePopulation").textContent =
+            "Could not load population data.";
+    }
+}
